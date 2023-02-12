0b41f6c8e02d2fc529b3b23d01121ed3704c2d04
antegral <antegral@antegral.net>
2023-02-12 21:42:01 +0900

    Add: title Add: underline

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/App.css";
+import ArrowIcon from "./icons/next-single-arrow.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="section">
+      <h1 className="pg1-title">Hunting the Interest things</h1>
+      <div className="pg1-area-underline">
+        <div className="pg1-box-blank"></div>
+        <div className="pg1-box-underline"></div>
+      </div>
+      <div className="pg1-area-scrolldown">
+        <img
+          className="pg1-icon-scrolldown"
+          src={ArrowIcon}
+          alt="Arrow Icon"
+        ></img>
+        <p className="pg1-text-scrolldown">아래로 넘기세요</p>
+      </div>
     </div>
   );
 }
+
+function GetSection() {}
 
 export default App;
