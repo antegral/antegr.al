7ab19125dbe05976d830e8129fba6683e633ded7
antegral <antegral@antegral.net>
2023-02-13 12:50:35 +0900

    Add: navbar Add: card switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,79 @@
 import React from "react";
+import Button, { ButtonProps } from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
 import "./css/App.css";
+import "./css/GetTitleSection.css";
+import "./css/GetNavBar.css";
 import ArrowIcon from "./icons/next-single-arrow.svg";
+import AntegralIcon from "./icons/antegral-9.png";
+
+const LoginButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  color: theme.palette.getContrastText("#00000"),
+}));
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 3,
+  borderRadius: 0,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === "dark" ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 0,
+    background:
+      "linear-gradient(90deg, rgb(73,233,255) 0%, rgb(146,135,255) 100%)",
+  },
+}));
 
 function App() {
   return (
-    <div className="section">
+    <div>
+      <GetNavBar />
+      <GetTitleSection />
+    </div>
+  );
+}
+
+function GetNavBar() {
+  return (
+    <div className="nav-section">
+      <img
+        className="nav-icon-antegral"
+        src={AntegralIcon}
+        alt="ANTEGRAL Icon"
+      ></img>
+
+      <div className="nav-area-innermenu">
+        <a className="nav-text-menu" href="https://blog.antegral.net">
+          BLOG
+        </a>
+        <a className="nav-text-menu" href="https://service.antegral.net">
+          SERVICE
+        </a>
+        <a className="nav-text-menu" href="https://server.antegral.net">
+          SERVER
+        </a>
+        <a className="nav-text-menu" href="https://github.com/antegral">
+          GITHUB
+        </a>
+        <a className="nav-text-menu" href="mailto:antegral@antegral.net">
+          CONTACT
+        </a>
+      </div>
+
+      <LoginButton variant="text" size="large">
+        SIGN IN
+      </LoginButton>
+    </div>
+  );
+}
+
+function GetTitleSection() {
+  return (
+    <div className="pg1-section">
       <h1 className="pg1-title">Hunting the Interest things</h1>
       <div className="pg1-area-underline">
         <div className="pg1-box-blank"></div>
@@ -18,10 +87,25 @@ function App() {
         ></img>
         <p className="pg1-text-scrolldown">아래로 넘기세요</p>
       </div>
+      <div className="pg1-area-cardstatus">
+        <p className="pg1-text-cardselect" id="card-btn-01">
+          01
+        </p>
+        <p className="pg1-text-cardunselect" id="card-btn-02">
+          02
+        </p>
+        <p className="pg1-text-cardunselect" id="card-btn-03">
+          03
+        </p>
+        <BorderLinearProgress
+          value={80}
+          variant="determinate"
+          title="test"
+          style={{ width: "10em", marginLeft: "1.2em" }}
+        />
+      </div>
     </div>
   );
 }
-
-function GetSection() {}
 
 export default App;
