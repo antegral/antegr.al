a9f8bbb7fb19f5ca2e1e573b564fc6064e3e7db9
antegral <antegral@antegral.net>
2023-09-01 21:49:12 +0900

    Add: label-tip

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,16 @@ function App() {
       <div className="card">
         <div className="card-container">
           <div className="label-container">
-            <div className="title-icon"></div>
-            <div className="label-name">SeongUk Moon</div>
-            <div className="label-nickname">ANTEGRAL</div>
+            <div className="icon-title"></div>
+            <p className="label-name">SeongUk Moon</p>
+            <p className="label-nickname">ANTEGRAL</p>
           </div>
+          <div className="vertical-hr"></div>
+          <div className="content-container"></div>
+          <div className="social-buttons"></div>
+          <p className="label-tip">
+            document.onmousedown = (event) =&gt; filpCard();
+          </p>
         </div>
       </div>
     </div>
